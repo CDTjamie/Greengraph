f071e4bf74f29b22fe167ca2adfe1e3a9ab43846
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-01-06 13:40:27 +0000

    Greengraph class from code appendix - tests added 3

--- greengraph/graph.py
+++ greengraph/graph.py
@@ -13,17 +13,29 @@ class Greengraph(object):
         self.geocoder = geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
         
     def geolocate(self, place):
+        if type(place) != str:
+            raise TypeError("Place name must be of type string")
+        
         return self.geocoder.geocode(place, exactly_one=False)[0][1]
     
     def location_sequence(self, start, end, steps):
+         if type(steps) != int:
+            raise TypeError("No. steps must be an integer")
+        if float(steps) <= 0:
+            raise ValueError("No. steps must be a positive integer")
+        if type(start) != list or type(end) != list:
+            raise TypeError("start, end input to location sequence incorrect type")
+        if len(start) != 2 or len(end) != 2:
+            raise ValueError("start, end input to location sequence must be array of length 2")
+        
         lats = np.linspace(start[0], end[0], steps)
         longs = np.linspace(start[1], end[1], steps)
         return np.vstack([lats, longs]).transpose()
     
     def green_between(self, steps):
         if type(steps) != int:
-            raise TypeError("No. steps should be an integer")
+            raise TypeError("No. steps must be an integer")
         if float(steps) <= 0:
-            raise ValueError("No.steps should be a positive integer")
-            
+            raise ValueError("No. steps must be a positive integer")
+        
         return [Map(*location).count_green() for location in self.location_sequence(self.geolocate(self.start), self.geolocate(self.end), steps)]
