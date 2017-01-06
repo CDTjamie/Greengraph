fd30a433efd41c5a7ffce2065878aa98a639598e
Jamie Potter <jamie.potter.16@ucl.ac.uk>
2017-01-06 12:08:48 +0000

    Unit tests - 2

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,6 +1,11 @@
 from nose.tools import assert_raises
 from greengraph import Greengraph
 
+def test_locations():
+    with assert_raises(TypeError) as exception: Greengraph('London', 6)
+    with assert_raises(TypeError) as exception: Greengraph(6.3, 'Oxford')
+    with assert_raises(TypeError) as exception: Greengraph([2, 'ghf'], 8)
+
 def test_steps_int():
     with assert_raises(TypeError) as exception: Greengraph('London', 'Oxford').green_between(10.2)
 
